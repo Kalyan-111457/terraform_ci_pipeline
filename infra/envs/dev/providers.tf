e44b180097726e592482ad018b5f3edc618568ca
Malisetty Kalyana Raghava <kalyan.111457@gmail.com>
2026-05-26 18:31:01 -0700

    Add dev Azure provider IDs

--- infra/envs/dev/providers.tf
+++ infra/envs/dev/providers.tf
@@ -9,4 +9,7 @@ terraform {
 
 provider "azurerm" {
   features {}
+  subscription_id = "93590cca-bced-4380-8577-63234a070e93"
+  client_id       = "84c9238d-90b4-43a2-abe9-55d13bc29a31"
+  tenant_id       = "c1cc912e-a245-46cc-a8cd-406ddf930a19"
 }
