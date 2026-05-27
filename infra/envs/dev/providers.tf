1d68715b70437d79188b1221138ea0fa0cc7fc23
Malisetty Kalyana Raghava <kalyan.111457@gmail.com>
2026-05-26 18:26:37 -0700

    sample

--- infra/envs/dev/providers.tf
+++ infra/envs/dev/providers.tf
@@ -9,8 +9,4 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "9202fdd4-3473-44bd-bf2c-bea84beb99d9"
-  client_id       = "b7998bce-a467-4e17-9150-87c9d08c82da"
-  client_secret   = "gto8Q~WXgOf~iYiAjUG30ZDaA2jvdIRbudu1IbiV"
-  tenant_id       = "2ed7f2e6-e8a9-4bc5-8cd6-cea790964ecc"
 }
